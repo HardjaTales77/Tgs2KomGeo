de0fbcc668b537a1d8c1058fe55fe2a6b39f5a30
HardjaTales77 <48974578+HardjaTales77@users.noreply.github.com>
2020-11-11 20:05:47 +0700

    setup

---
+++ Tugas2KomGeo/src/tugas2komgeo/Point.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas2komgeo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Point {
+    double x; //kordinat x
+    double y; //kordinat y
+  
+    public Point(double x, double y){
+        this.x=x; //isi x
+        this.y=y; //isi y
+    }
+}
